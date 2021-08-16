b9f977c868e572d70b7054fcddba6e9f97394da0
Rotciv <rotciv_ocnarb@hotmail.com>
2021-08-16 09:22:09 -0300

    next level

--- Runtime/LevelSelection/Scripts/WorldCollection.cs
+++ Runtime/LevelSelection/Scripts/WorldCollection.cs
@@ -4,5 +4,28 @@ namespace AurecasLib.Levels {
     [CreateAssetMenu(fileName = "World Collection", menuName = "AurecasLib/Worlds/World Collection")]
     public class WorldCollection : ScriptableObject {
         public WorldSO[] worlds;
+
+        public bool GetNextLevel(int world, int level, out int nextWorld, out int nextLevel) {
+
+            int nl = level + 1;
+            int nw = world;
+
+            if(nl >= worlds[world].levels.Count) {
+                nl = 0;
+                nw++;
+            }
+
+            if(nw >= worlds.Length) {
+                nw = 0;
+                nextWorld = nw;
+                nextLevel = nl;
+                return false;
+            }
+
+            nextWorld = nw;
+            nextLevel = nl;
+            return true;
+
+        }
     }
 }
