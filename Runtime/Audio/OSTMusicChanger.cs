5320510d5e6b202ad99f202ff4b0cb2410337e7f
Rotciv <rotciv_ocnarb@hotmail.com>
2021-08-17 09:13:42 -0300

    sound module

---
+++ Runtime/Audio/OSTMusicChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OSTMusicChanger : MonoBehaviour
+{
+    public AudioClip musicToChange;
+
+    public void Start() {
+        if (OSTManager.Instance) {
+            OSTManager.Instance.PlayClip(musicToChange);
+        }
+    }
+}
